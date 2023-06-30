669787bd6229b00c75d263ef10306e2109016978
yuwee <wiiiru26@gmail.com>
2023-06-30 21:28:10 +0500

    bruh

--- MonsterBuilder/Assets/Scripts/DemoColorPicker.cs
+++ MonsterBuilder/Assets/Scripts/DemoColorPicker.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class DemoColorPicker : MonoBehaviour
+public class DemoColorPicker : MonoBehaviour
+{
+    [SerializeField] private GameObject _activePart;
+
+    public void SetActive(GameObject activePart) 
     {
-        public void SetColor(Color newColor)
-        {
-            GetComponent<SpriteRenderer>().color = newColor;
-            Debug.Log(newColor);
-        }
+        _activePart = activePart;
     }
+
+    public void SetColor(Color newColor)
+    {
+        SpriteRenderer renderer = _activePart.GetComponent<SpriteRenderer>();
+        if (renderer == null) 
+        {
+            renderer = _activePart.GetComponent<Transform>().GetChild(0).GetComponent<SpriteRenderer>();
+        }
+        renderer.color = newColor;
+        Debug.Log(newColor);
+    }
+}
